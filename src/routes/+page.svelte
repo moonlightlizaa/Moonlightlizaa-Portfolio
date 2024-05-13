ca415bd83a0d3f95f715f34e9c711b6f21bc9a8b
eliza <eliza.lacroes@gmail.com>
2024-05-13 15:40:24 +0200

    ✨ 3D model added
    yay!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+
+<header>
+    <section class="scene">
+        <section class="home">
+            <section class="canvas">
+                    <script type="module" src="https://unpkg.com/@splinetool/viewer@1.3.1/build/spline-viewer.js"></script>
+                    <spline-viewer loading-anim-type="spinner-small-dark" url="https://prod.spline.design/M5QYIxncM4JfLx4i/scene.splinecode"></spline-viewer>
+                <section class="canvas-gradient">
+                </section>
+            </section>
+        </section>
+        <section class="header-intro">
+            <section class="header-intro-title"> 
+                <h1>moonlightlizaa</h1>
+            </section>
+            <section class="header-intro-bio">
+                <p>hello! i’m eliza — a student frontend developer & designer
+                </p>
+            </section>
+        </section>
+    </section>
+
+
+</header>
