a92a9c873371530b53d14fe13fe6fdf60f3bdc7d
eliza <eliza.lacroes@gmail.com>
2024-05-14 19:14:39 +0200

    ❌ reset

--- moonlightlizaa-portfolio/src/routes/api/preview/+server.js
+++
@@ -1,8 +0,0 @@
-import { redirectToPreviewURL } from '@prismicio/svelte/kit';
-import { createClient } from '$lib/prismicio.js';
-
-export async function GET({ fetch, request, cookies }) {
-	const client = createClient({ fetch });
-
-	return await redirectToPreviewURL({ client, request, cookies });
-}
